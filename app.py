54999887d5d390eb673efc20404bf6553503da9b
Gideon-Ogunbanjo <dejiogunbanjo9@gmail.com>
2023-05-25 15:47:13 +0100

    created variables that takes user input

--- app.py
+++ app.py
@@ -6,6 +6,50 @@ import pandas as pd
 
 #load the model and dataframe
 df = pd.read_csv("bodyfat.csv")
-pipe = pickle.load(open("pipe.pkl", "rb"))
+# pipe = pickle.load(open("pipe.pkl", "rb"))
 
-st.title("Supermodel Prediction Model")
+st.title("ModelX - Supermodel Prediction Model")
+
+# taking user input
+
+#age
+age = st.selectbox('Age: ', df['Age'].unique())
+
+# Density
+Density = st.selectbox("Density: ", df['Density'].unique())
+
+# weight
+weight = st.selectbox("Weight: ", df['Weight'].unique())
+
+# height
+height = st.selectbox("Height: ", df['Height'].unique())
+
+#neck
+neck = st.selectbox('Neck Size: ', df['Neck'].unique())
+
+# chest
+chest = st.selectbox("Chest Size: ", df['Chest'].unique())
+
+# abdomen
+abdomen = st.selectbox("Abdomen Size: ", df['Abdomen'].unique())
+
+# hip
+hip = st.selectbox("Hip Size: ", df['Hip'].unique())
+
+#thigh
+thigh = st.selectbox('Thigh Size: ', df['Thigh'].unique())
+
+# knee
+knee = st.selectbox("Knee Size", df['Knee'].unique())
+
+# ankle
+ankle = st.selectbox("Ankle Measurement", df['Ankle'].unique())
+
+# biceps
+biceps = st.selectbox("Bicep Size: ", df['Biceps'].unique())
+
+#forearm
+forearm = st.selectbox('Forearm Size: ', df['Forearm'].unique())
+
+# wrist
+wrist = st.selectbox("Wrist Size: ", df['Wrist'].unique())
