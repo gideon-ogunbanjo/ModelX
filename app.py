1778dbf90c0f115fdc61544886c996b5e32abc66
Gideon-Ogunbanjo <dejiogunbanjo9@gmail.com>
2023-05-25 15:34:14 +0100

    -

--- app.py
+++ app.py
@@ -3,3 +3,9 @@ import streamlit as st
 import numpy as np
 import pickle
 import pandas as pd
+
+#load the model and dataframe
+df = pd.read_csv("bodyfat.csv")
+pipe = pickle.load(open("pipe.pkl", "rb"))
+
+st.title("Supermodel Prediction Model")
