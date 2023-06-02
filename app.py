1c5a3192bd245c5408f70100accf65bcb317b02a
Gideon-Ogunbanjo <dejiogunbanjo9@gmail.com>
2023-06-02 23:10:01 +0100

    made minor changes

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import joblib
 
 #loading the model dataset ande pickle file
 df = pd.read_csv("bodyfat.csv")
-# pipe = pickle.load(open("pipe.pkl", "rb"))
+pipe = pickle.load(open("modelx.pkl", "rb"))
 
 with st.columns(3)[1]:
      st.title("ModelX")
@@ -58,12 +58,12 @@ forearm = st.number_input("Forearm Size: ")
 # wrist
 wrist = st.number_input("Wrist Size: ")
 
-# running predictions
+#running predictions
 
-# if st.button('Predict'):
-#     ppi = None
-#     query = np.array([age, density, weight, height, neck, chest, abdomen, hip, thigh, knee, ankle, biceps, forearm, wrist])
-#     query = query.reshape(1, 14)
-#     prediction = str(int(np.exp(pipe.predict(query)[0])))
-#     st.title("The predicted body fat perentage of this individual is: " + prediction)
-# st.markdown("Created by Gideon Ogunbanjo")
+if st.button('Predict'):
+    ppi = None
+    query = np.array([age, density, weight, height, neck, chest, abdomen, hip, thigh, knee, ankle, biceps, forearm, wrist])
+    query = query.reshape(1, 14)
+    prediction = str(int(np.exp(pipe.predict(query)[0])))
+    st.title("The predicted body fat perentage of this individual is: " + prediction)
+st.markdown("Created by Gideon Ogunbanjo")
