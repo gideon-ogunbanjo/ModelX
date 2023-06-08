13f32234031c0ee9ed193da69c910390d711eceb
Gideon-Ogunbanjo <dejiogunbanjo9@gmail.com>
2023-06-08 03:11:05 +0100

    got rid of errors for now

--- app.py
+++ app.py
@@ -1,69 +1,69 @@
-# importing technologies
-import streamlit as st
-import numpy as np
-import pickle
-import pandas as pd
-from sklearn.linear_model import LinearRegression
-from sklearn.compose import ColumnTransformer
-import joblib
+# # importing technologies
+# import streamlit as st
+# import numpy as np
+# import pickle
+# import pandas as pd
+# from sklearn.linear_model import LinearRegression
+# from sklearn.compose import ColumnTransformer
+# import joblib
 
-#loading the model dataset ande pickle file
-df = pd.read_csv("bodyfat.csv")
-pipe = pickle.load(open("modelx.pkl", "rb"))
+# #loading the model dataset ande pickle file
+# df = pd.read_csv("bodyfat.csv")
+# pipe = pickle.load(open("modelx.pkl", "rb"))
 
-with st.columns(3)[1]:
-     st.title("ModelX")
-st.markdown("Enter user parameters")
-# -- Taking user input -- 
+# with st.columns(3)[1]:
+#      st.title("ModelX")
+# st.markdown("Enter user parameters")
+# # -- Taking user input -- 
 
-#age
-age = st.number_input('Age: ')
+# #age
+# age = st.number_input('Age: ')
 
-# Density
-density = st.number_input("Density: ")
+# # Density
+# density = st.number_input("Density: ")
 
-# weight
-weight = st.number_input("Weight: ")
+# # weight
+# weight = st.number_input("Weight: ")
 
-# height
-height = st.number_input("Height: ")
+# # height
+# height = st.number_input("Height: ")
 
-#neck
-neck = st.number_input("Neck Size: ")
+# #neck
+# neck = st.number_input("Neck Size: ")
 
-# chest
-chest = st.number_input("Chest Size: ")
+# # chest
+# chest = st.number_input("Chest Size: ")
 
-# abdomen
-abdomen = st.number_input("Abdomen Size: ")
+# # abdomen
+# abdomen = st.number_input("Abdomen Size: ")
 
-# hip
-hip = st.number_input("Hip Size: ")
+# # hip
+# hip = st.number_input("Hip Size: ")
 
-#thigh
-thigh = st.number_input("Thigh Size: ")
+# #thigh
+# thigh = st.number_input("Thigh Size: ")
 
-# knee
-knee = st.number_input("Knee Size: ")
+# # knee
+# knee = st.number_input("Knee Size: ")
 
-# ankle
-ankle = st.number_input("Ankle Measurement: ")
+# # ankle
+# ankle = st.number_input("Ankle Measurement: ")
 
-# biceps
-biceps = st.number_input("Bicep Size: ")
+# # biceps
+# biceps = st.number_input("Bicep Size: ")
 
-#forearm
-forearm = st.number_input("Forearm Size: ")
+# #forearm
+# forearm = st.number_input("Forearm Size: ")
 
-# wrist
-wrist = st.number_input("Wrist Size: ")
+# # wrist
+# wrist = st.number_input("Wrist Size: ")
 
-#running predictions
+# #running predictions
 
-if st.button('Predict'):
-    ppi = None
-    query = np.array([age, density, weight, height, neck, chest, abdomen, hip, thigh, knee, ankle, biceps, forearm, wrist])
-    query = query.reshape(1, 14)
-    prediction = str(int(np.exp(pipe.predict(query)[0])))
-    st.title("The predicted body fat perentage of this individual is: " + prediction)
-st.markdown("Created by Gideon Ogunbanjo")
+# if st.button('Predict'):
+#     ppi = None
+#     query = np.array([age, density, weight, height, neck, chest, abdomen, hip, thigh, knee, ankle, biceps, forearm, wrist])
+#     query = query.reshape(1, 14)
+#     prediction = str(int(np.exp(pipe.predict(query)[0])))
+#     st.title("The predicted body fat perentage of this individual is: " + prediction)
+# st.markdown("Created by Gideon Ogunbanjo")
