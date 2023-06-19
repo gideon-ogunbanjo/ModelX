7a1bcb9b333a8382c13453f423f3816d19eafb07
Gideon-Ogunbanjo <dejiogunbanjo9@gmail.com>
2023-06-19 22:36:24 +0100

    built the main model

--- app.py
+++ app.py
@@ -14,3 +14,62 @@ def predict_fat(Density, Age, Weight, Height, Neck, Chest, Abdomen, Hip, Thigh, 
     
     return int(prediction)
 
+# creating the main function
+def main():
+    st.title("ModelX - Supermodel Prediction Model")
+    html_temp = """
+    <div style="background:#025246 ;padding:10px">
+    <h2 style="color:white;text-align:center;"> ModelX </h2>
+    </div>
+    """
+    st.markdown(html_temp, unsafe_allow_html = True)
+    
+    #taking user input
+    Density = st.text_input("Density","Type Here")
+    Age = st.text_input("Age","Type Here")
+    Weight = st.text_input("Weight","Type Here")
+    Height = st.text_input("Height","Type Here")
+    Neck = st.text_input("Neck","Type Here")
+    Chest = st.text_input("Chest","Type Here")
+    Abdomen = st.text_input("Abdomen","Type Here")
+    Hip = st.text_input("Hip","Type Here")
+    Thigh = st.text_input("Thigh","Type Here")
+    Knee = st.text_input("Knee","Type Here")
+    Ankle = st.text_input("Ankle","Type Here")
+    Biceps = st.text_input("Biceps","Type Here")
+    Forearm = st.text_input("Forearm","Type Here")
+    Wrist = st.text_input("Wrist","Type Here")
+    safe_html ="""  
+        <div style="background-color:#80ff80; padding:10px >
+        <h2 style="color:white;text-align:center;"> The individual is Fantastic for modeling</h2>
+        </div>
+        """
+    safe_html ="""  
+      <div style="background-color:#80ff80; padding:10px >
+      <h2 style="color:white;text-align:center;"> The Individual is suitable</h2>
+      </div>
+    """
+    warn_html ="""  
+      <div style="background-color:#F4D03F; padding:10px >
+      <h2 style="color:white;text-align:center;"> The individual is managable</h2>
+      </div>
+    """
+    danger_html="""  
+      <div style="background-color:#F08080; padding:10px >
+       <h2 style="color:black ;text-align:center;"> The individual is not suitable</h2>
+       </div>
+    """
+
+    if st.button("Predict the BodyFat"):
+        output = predict_fat(Density, Age, Weight, Height, Neck, Chest, Abdomen, Hip, Thigh, Knee, Ankle, Biceps, Forearm, Wrist)
+        st.success('The age is {}'.format(output))
+
+        if output == 1:
+            st.markdown(safe_html,unsafe_allow_html=True)
+        elif output == 2:
+            st.markdown(warn_html,unsafe_allow_html=True)
+        elif output == 3:
+            st.markdown(danger_html,unsafe_allow_html=True)
+
+if __name__=='__main__':
+    main()
