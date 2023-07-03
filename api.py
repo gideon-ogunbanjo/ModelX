5e9414c8dc7b50c5c530d78630a0d89daf645670
Gideon-Ogunbanjo <dejiogunbanjo9@gmail.com>
2023-07-03 13:43:17 +0100

    reverted files

--- api.py
+++ api.py
@@ -2,16 +2,8 @@
 from flask import Flask,jsonify,request
 import joblib
 app = Flask(__name__)
-
-@app.route("/", methods=['POST','GET'])
-def index():
-    if(request.method == 'POST'):
-        data = request.get_json()
-        body_fat = float(data["labels"])
-        lin_reg = joblib.load("modelx.pkl")
-        return jsonify(lin_reg.predict([[body_fat]]).tolist())
-    else:
-        return  jsonify({"about":"Hello World"})
-    
+@app.route("/")
+def hello():
+    return "Hello World!"
 if __name__ == '__main__':
     app.run(debug=True)
