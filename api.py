944c1c8e9f60abd23d34af01dff647f717763d37
Gideon-Ogunbanjo <dejiogunbanjo9@gmail.com>
2023-07-03 13:43:50 +0100

    reverted the last commit

--- api.py
+++
@@ -1,9 +0,0 @@
-# importing necessary libraries
-from flask import Flask,jsonify,request
-import joblib
-app = Flask(__name__)
-@app.route("/")
-def hello():
-    return "Hello World!"
-if __name__ == '__main__':
-    app.run(debug=True)
