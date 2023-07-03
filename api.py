27beb36e5c8d003d6a085461c2bc284cbdc6d2e5
Gideon-Ogunbanjo <dejiogunbanjo9@gmail.com>
2023-07-03 13:42:28 +0100

    #

--- api.py
+++ api.py
@@ -2,8 +2,16 @@
 from flask import Flask,jsonify,request
 import joblib
 app = Flask(__name__)
-@app.route("/")
-def hello():
-    return "Hello World!"
+
+@app.route("/", methods=['POST','GET'])
+def index():
+    if(request.method == 'POST'):
+        data = request.get_json()
+        body_fat = float(data["labels"])
+        lin_reg = joblib.load("modelx.pkl")
+        return jsonify(lin_reg.predict([[body_fat]]).tolist())
+    else:
+        return  jsonify({"about":"Hello World"})
+    
 if __name__ == '__main__':
     app.run(debug=True)
