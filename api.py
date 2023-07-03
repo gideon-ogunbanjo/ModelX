271c474904387b4b4b2e09a16b53d33470b07771
Gideon-Ogunbanjo <dejiogunbanjo9@gmail.com>
2023-07-03 13:44:48 +0100

    reverted back to commit 9dc6adb

---
+++ api.py
@@ -0,0 +1,56 @@
+# Importing Necessary modules
+import numpy as np
+import pandas as pd
+import pickle as pkl
+from sklearn.linear_model import LinearRegression
+from pydantic import BaseModel
+from fastapi import FastAPI
+import uvicorn
+
+# Declaring our FastAPI instance
+app = FastAPI()
+# Creating class to define the request body
+# and the type hints of each attribute
+class request_body(BaseModel):
+    Density : float
+    Body_mass_Index : float
+    Neck : float
+    Chest : float
+    Abdomen : float
+    Hip : float
+    Thigh : float
+    Knee : float
+    Ankle : float
+    Biceps : float
+    Forearm : float
+    Wrist : float
+# Defining path operation for root endpoint
+# Loading file
+data = pd.read_pickle('modelX.pkl')
+
+# Getting features and targets from the dataset
+X = data.features
+y = data.target
+
+# Fitting our Model on the dataset
+model = LinearRegression()
+model.fit(X,y)
+
+# Creating an Endpoint to receive the data to make prediction on.
+@app.post('/predict')
+def predict(data : request_body):
+    # Making the data in a form suitable for prediction
+    test_data = [[
+            data.Density,
+            data.Body_mass_Index,
+            data.Neck,
+            data.Chest,
+            data.Abdomen,
+            data.Hip,
+            data.Thigh,
+            data.Knee,
+            data.Ankle,
+            data.Biceps,
+            data.Forearm,
+            data.Wrist
+    ]]
